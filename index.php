d2e0564f7a280f328b56d5468b6ee081de464d08
williamj2543 <williamj2543@gmail.com>
2016-06-24 18:38:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <title>Lab Generator</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+<form action="index.php">
 <div class="form-group">
   <label for="comment">Paste Data from excel:</label>
-  <textarea class="form-control" rows="20" id="comment"></textarea>
+  <textarea class="form-control" rows="20" id="data"></textarea>
 </div>
+</form>
 <?php
-echo "hello world2";
+if(!empty($_POST)){
+  print_r($_POST['data']);
+}
 ?>
