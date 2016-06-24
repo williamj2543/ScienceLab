18b41184eb7c7fe9d6c41c04116456fed64b82a7
williamj2543 <williamj2543@gmail.com>
2016-06-24 18:48:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,12 @@
 </form>
 <?php
 if(!empty($_POST)){
-  print_r($_POST['data']);
+  $rows = array_map('str_getcsv', $_POST['data']);
+$header = array_shift($rows);
+$csv = array();
+foreach ($rows as $row) {
+   $csv[] = array_combine($header, $row);
+}
+print_r($csv);
 }
 ?>
